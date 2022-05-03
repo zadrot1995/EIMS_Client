e9ab0a823c60b90d688705555f4bf6667f48cf73
Andriy_Antonik <zadrotdd43@gmail.com>
2022-05-03 20:44:03 +0300

    adding students by excel

--- src/app/components/student/add-students/add-students.component.ts
+++ src/app/components/student/add-students/add-students.component.ts
@@ -18,12 +18,17 @@ type AOA = any[][];
 })
 export class AddStudentsComponent {
 
+  data: AOA;
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
+  isManualAdding = true;
   selectedFile = null;
   student = new Student();
   result: object;
   constructor(public httpBaseService: HttpBaseService,
               private router: Router,
-              private route: ActivatedRoute) {}
+              private route: ActivatedRoute,
+              private studentsService: StudentsService) {}
 
   addTeacher(){
     this.httpBaseService.Post(this.student, ApiRouts.students).subscribe(x =>
@@ -35,5 +40,39 @@ export class AddStudentsComponent {
   ngOnInit(): void {
     this.student.groupId = Guid.parse(this.route.snapshot.paramMap.get('groupId')).toString();
 
+  }
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      let students: Student[];
+      students = this.studentsService.XlsxToStudents(this.data);
+      console.log(students);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+  export(): void {
+    /* generate worksheet */
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
   }
 }
--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Student} from "../Models/Student";
+import * as XLSX from "xlsx";
 
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -10,11 +12,18 @@ const httpOptions = {
 })
 export class StudentsService {
   constructor(private http: HttpClient) {}
-  public addNewStudents(body){
-    const result = this.http.post('https://localhost:44304/api/students/test', body, httpOptions).subscribe(data => {
-      console.log(data);
-      console.log(body);
-    });
-    return result;
+  public XlsxToStudents(data){
+    let students: Student[] = [];
+    let i = 0;
+    for (let row of data) {
+      let student = new Student();
+      if (i !== 0) {
+        student.firstName = row[0];
+        student.secondName = row [0];
+        students.push(student);
+      }
+      i++;
+    }
+    return students;
   }
 }
